d6b7c00f05cc8e48ab1946da580243fca235ff83
kurunve <volodymyrantonuk@gmail.com>
2014-10-26 00:32:36 +0300

    metakoans 1-5 done

--- knowledge.rb
+++ knowledge.rb
@@ -1,5 +1,11 @@
 class Module
     def attribute a
-       @a = a
+       eval "define_method :#{a} do @#{a}; end"
+       eval "define_method :#{a}= do |param| @#{a}=param; end"
+       eval "define_method :#{a}? do defined? @#{a}; end"
+
+       #instance_eval "def #{a}; @#{a}; end"
+       #instance_eval "def #{a}= param; @#{a} = param; end"
+       #instance_eval "def #{a}?; @#{a}.nil?; end"
     end
 end
